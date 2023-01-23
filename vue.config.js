e1da6fabcbe6bf2800f0a64d890f284c698e8a83
gnemek20 <gnemek20@gmail.com>
2023-01-23 21:01:00 +0900

    환경설정

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    allowedHosts: "all",
+    port: 80,
+    proxy:{
+      "/api": {
+        target: "http://localhost:3000/api",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api": ''
+        }
+      }
+    }
+  }
 })
